2edb5bdd09f6bfdc19a0154d7555595c20eb3bbd
yaskaboy <109895325+yaasiinaxmed@users.noreply.github.com>
2022-12-08 10:45:56 +0300

    updated all

--- search/main.js
+++ search/main.js
@@ -1,7 +1,9 @@
 const searchInput = document.querySelector("#search");
+const boxContainer = document.querySelector(".boxs");
 const items = document.querySelectorAll(".box");
 
 searchInput.addEventListener("keyup", searchItem);
+boxContainer.addEventListener("scroll", itemAnimated);
 
 function searchItem() {
     const inputText = searchInput.value.toLowerCase();
@@ -13,6 +15,20 @@ function searchItem() {
             item.style.display = 'block';
         } else {
             item.style.display = 'none';
+        };
+    });
+};
+
+function itemAnimated() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    items.forEach(item => {
+        const itemTop = item.getBoundingClientRect().top;
+    
+        if (triggerBottom > itemTop) {
+            item.classList.add("show");
+        } else {
+            item.classList.remove("show");
         };
     });
 };
