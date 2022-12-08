1b1a69bc748ec44abcdc31cef08125538229ee24
yaskaboy <109895325+yaasiinaxmed@users.noreply.github.com>
2022-12-08 07:11:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const links = document.querySelectorAll("header nav ul li a");
 const sections = document.querySelectorAll("section");
 
+window.addEventListener("scroll", () => {
+    let id = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop) {
+            id = section.getAttribute("id");
+        }
+    });
+});
