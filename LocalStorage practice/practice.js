c04027bf760b2c9cac5cf8fc18cb0c2db2bd0342
yaskaboy <109895325+yaasiinaxmed@users.noreply.github.com>
2022-12-09 09:26:31 +0300

    updated all

---
+++ LocalStorage practice/practice.js
@@ -0,0 +1,53 @@
+const studentForm = document.querySelector("#studentForm");
+const studentsContainer = document.querySelector(".students");
+const nameInput = studentForm['name'];
+const ageInput = studentForm['age'];
+const rollInput = studentForm['roll'];
+
+const students = JSON.parse(localStorage.getItem("students")) || [];
+
+const addStudent = (name, age, roll) => {
+    students.push(
+        {
+            name: name,
+            age: age,
+            roll: roll,
+        },
+    );
+
+    localStorage.setItem("students", JSON.stringify(students));
+
+    return {name , age, roll};
+};
+
+const createStudentElement = ({name, age, roll}) => {
+    const studentDiv = document.createElement("div");
+    const studentName = document.createElement("h2");
+    const studentAge = document.createElement("p");
+    const studentRoll = document.createElement("P");
+
+    studentName.innerText = "Student name: " + name;
+    studentAge.innerText = "Student age: " + age;
+    studentRoll.innerText = "Student roll: " + roll;
+
+    studentDiv.append(studentName, studentAge, studentRoll);
+    studentsContainer.appendChild(studentDiv);
+};
+
+students.forEach(createStudentElement);
+
+studentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const newStudent = addStudent(
+        nameInput.value,
+        ageInput.value,
+        rollInput.vlaue
+    );
+
+    createStudentElement(newStudent);
+
+    nameInput.value = "";
+    ageInput.value = "";
+    rollInput.value = "";
+});
